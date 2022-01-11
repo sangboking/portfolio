2efa2ba6a90aa1e7cbff3e911e0ff299429d3424
sangboking <cowg001@naver.com>
2022-01-11 15:26:51 +0900

    하단 blog 로그 추가,Movie App추가

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,24 +1,28 @@
 import "./contact.scss"
 import { AiFillGithub,AiFillInstagram,AiFillFacebook,AiFillMail} from "react-icons/ai";
-
+import { FaBloggerB } from "react-icons/fa";
 export default function Contact() {
   return (
     <div className="contact" id="Contact">
       <div className="content">
         <div className="git">
-          <a href="https://github.com/sangboking"><AiFillGithub/></a>
+          <a href="https://github.com/sangboking" target='_blank'><AiFillGithub/></a>
         </div>
 
         <div className="instar">
-          <a href="https://www.instagram.com/ssanghoons/?hl=ko"><AiFillInstagram/></a>
+          <a href="https://www.instagram.com/ssanghoons/?hl=ko" target='_blank'><AiFillInstagram/></a>
         </div>
 
         <div className="facebook">
-          <a href="https://www.facebook.com/profile.php?id=100005739102512"><AiFillFacebook/></a>
+          <a href="https://www.facebook.com/profile.php?id=100005739102512" target='_blank'><AiFillFacebook/></a>
         </div>
 
         <div className="mail">
-          <a href="mailto:cowg001@naver.com"><AiFillMail/></a>
+          <a href="mailto:cowg001@naver.com" target='_blank'><AiFillMail/></a>
+        </div>
+
+        <div className="blog">
+          <a href="https://sanghoonfrontend.tistory.com/" target='_blank'><FaBloggerB/></a>
         </div>
 
       </div>
--- src/components/projects/ProjectData.jsx
+++ src/components/projects/ProjectData.jsx
@@ -63,5 +63,18 @@ export default [
     pagead:"https://sangboking.github.io/todolist/",
     page:"sangboking.github.io/todolist/",
     skill:"React, Scss"
+  },
+  {
+    id:6,
+    title:"Movie App",
+    date:"제작기간 : 2021.1월",
+    content:"React를 사용한 Movie App 입니다.",
+    content2:"Movie API를 받아 화면에 간단하게 구현한 사이트입니다.비동기 처리인 async await에 대해 깊게학습할수 있었고, react-router-dom의 6버전이 나오면서 바뀐 내용을 적용하여 사용하였습니다.",
+    function:"API처리, Router처리",
+    githubad:"https://github.com/sangboking/todolist",
+    github:"github.com/sangboking/react-movieapp",
+    pagead:"https://sangboking.github.io/react-movieapp/",
+    page:"sangboking.github.io/react-movieapp/",
+    skill:"React, Scss"
   }
 ]
